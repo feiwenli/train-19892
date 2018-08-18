b76522eff4c83a822f7e48dc8c66c8f2422253dd
feiwenli <lfw_dar@163.com>
2018-08-18 12:38:37 +0800

    [ADD] 2

--- src/main/java/io/choerodon/demo/api/TrController.java
+++ src/main/java/io/choerodon/demo/api/TrController.java
@@ -1,8 +1,5 @@
 package io.choerodon.demo.api;
 
-import io.choerodon.core.iam.ResourceLevel;
-import io.choerodon.swagger.annotation.Permission;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +13,8 @@ public class TrController {
     @Value("${name}")
     private String name;
 
-    @Permission(level = ResourceLevel.SITE, permissionPublic = true)
-    @ApiOperation(value = "demo")
+//    @Permission(level = ResourceLevel.SITE, permissionPublic = true)
+//    @ApiOperation(value = "demo")
     @GetMapping("/hello")
     public String test01(){
         return "Hello, "+number+", "+name;
